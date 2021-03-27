b5de4a7b77ac4639ced2c199e75b1acceb42ac57
TheBardockGames <jared12_genaro21@hotmail.com>
2021-03-27 10:20:17 -0600

    Revert "GOS2-TOOLS"
    This reverts commit 3dd1e2ad505f502b01d1e7701ae629c472e68406.

--- NGE ANIMA GAME TOOLS/Dialogos.Designer.cs
+++ NGE ANIMA GAME TOOLS/Dialogos.Designer.cs
@@ -38,9 +38,6 @@ namespace NGE_ANIMA_GAME_TOOLS
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -140,45 +137,15 @@ namespace NGE_ANIMA_GAME_TOOLS
             // 
             this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(43)))), ((int)(((byte)(0)))));
             this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox2.Font = new System.Drawing.Font("Arial", 13.5F);
+            this.textBox2.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(200)))), ((int)(((byte)(0)))));
             this.textBox2.ImeMode = System.Windows.Forms.ImeMode.Off;
-            this.textBox2.Location = new System.Drawing.Point(29, 332);
+            this.textBox2.Location = new System.Drawing.Point(34, 332);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
             this.textBox2.ReadOnly = true;
             this.textBox2.Size = new System.Drawing.Size(480, 88);
             this.textBox2.TabIndex = 6;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(492, 166);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(28, 23);
-            this.button2.TabIndex = 7;
-            this.button2.Text = ">";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(407, 166);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(49, 23);
-            this.button3.TabIndex = 9;
-            this.button3.Text = "restart";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(462, 166);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(28, 23);
-            this.button4.TabIndex = 10;
-            this.button4.Text = "<";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // Dialogos
             // 
@@ -186,9 +153,6 @@ namespace NGE_ANIMA_GAME_TOOLS
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(81)))), ((int)(((byte)(180)))));
             this.ClientSize = new System.Drawing.Size(671, 450);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.pictureBox1);
@@ -215,8 +179,5 @@ namespace NGE_ANIMA_GAME_TOOLS
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
     }
 }
--- NGE ANIMA GAME TOOLS/Dialogos.cs
+++ NGE ANIMA GAME TOOLS/Dialogos.cs
@@ -8,23 +8,16 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
-using YamlDotNet.Core;
 
 namespace NGE_ANIMA_GAME_TOOLS
 {
-    
     public partial class Dialogos : Form
     {
-        public const string Pathgame = @"C:\Program Files (x86)\GAINAX\IRONMAIDEN2\";
-        public const string palabra_buscar = "print";
-        public string listastring;
-        public string linea;
-
         public Dialogos()
         {
             InitializeComponent();
             listBoxtxt.Items.Clear();
-            string folder = Pathgame;
+            string folder = @"C:\Program Files (x86)\GAINAX\IRONMAIDEN2";
             string[] txtfiles = Directory.GetFiles(folder, "*.txt");
 
             foreach (var item in txtfiles)
@@ -33,116 +26,32 @@ namespace NGE_ANIMA_GAME_TOOLS
             }
         }
 
-        int counter = 0;
-        int linecounter = 0;
-        List<string> lista = new List<string>();
-        string line;
-
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
             var texto = textBox1.Text.Replace("print", "");
             var texto1 = texto.Replace("@c", "");
             var texto2 = texto1.Replace("\"", "");
-            string texto3 = texto2.Trim();
-            textBox2.Text = texto3;
-            string arroba = "@n";
-            if (textBox2.Text.Contains(arroba))
-            {
-                var sinarroba = texto3.Replace("@n", System.Environment.NewLine);
-                textBox2.Text = sinarroba;
-
-            }
+            textBox2.Text = texto2;
         }
 
 
-        public void listBoxtxt_SelectedIndexChanged(object sender, EventArgs e)
+        private void listBoxtxt_SelectedIndexChanged(object sender, EventArgs e)
         {
-            System.IO.StreamReader file = new System.IO.StreamReader(Pathgame + listBoxtxt.SelectedItem);
-            
-            while((line = file.ReadLine()) != null)
+            string result = string.Empty;
+            var lines = File.ReadAllLines(@"C:\Program Files (x86)\GAINAX\IRONMAIDEN2\" + listBoxtxt.SelectedItem);
+            foreach (var line in lines)
             {
-                if (line.Contains(palabra_buscar))
+                if (line.Contains("print"))
                 {
-                    lista.Add(counter.ToString());
-                    listastring = string.Join(Environment.NewLine, lista);
-                    string numberline = listastring.Split("\n".ToCharArray())[linecounter];
-                    int linea1 = Convert.ToInt32(numberline);
-                    string line = File.ReadLines(Pathgame + listBoxtxt.SelectedItem).Skip(linea1).Take(1).First();
-                    textBox1.Text = line;
-                }
-                counter++;
-
-            }
-
-            
-            file.Close();
-
-            /*
-            string cadena = File.ReadAllText(Pathgame + listBoxtxt.SelectedItem);
-            string[] palabras = cadena.Split();
-            string op = "";
-            int index = -1;
-            do
-            {
-                index = buscarPalabra(palabras, index + 1);
-                if (index != -1)
-                {
-                    MessageBox.Show("Se encontro print " + index);
-                }
-            } while (index != -1);
-            */
-        }
-
-        public static int buscarPalabra(String[] palabras, int index)
-        {
-            for (int i = index; i < palabras.Length; i++)
-            {
-                if (palabras[i].Contains("print"))
-                {
-                    return i;
+                    
+                    var text = line.ToString();
+                    textBox1.Text = text;
+                    var texto = textBox1.Text.Replace("print", "");
+                    var texto1 = texto.Replace("@c", "");
+                    var texto2 = texto1.Replace("\"", "");
+                    textBox2.Text = texto2;
                 }
             }
-            return -1;
         }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            /*
-            string line1 = listastring.Split(new[] { '\r', '\n' }).FirstOrDefault();
-            MessageBox.Show(line1);
-            */
-            linecounter++;
-            string numberline = listastring.Split("\n".ToCharArray())[linecounter];
-            int linea1 = Convert.ToInt32(numberline);
-            string line = File.ReadLines(Pathgame + listBoxtxt.SelectedItem).Skip(linea1).Take(1).First();
-            textBox1.Text = line;
-            
-
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            lista.Clear();
-            linecounter = 0;
-            counter = 0;
-            listastring = string.Empty;
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-            linecounter--;
-            string numberline = listastring.Split("\n".ToCharArray())[linecounter];
-            int linea1 = Convert.ToInt32(numberline);
-            string line = File.ReadLines(Pathgame + listBoxtxt.SelectedItem).Skip(linea1).Take(1).First();
-            textBox1.Text = line;
-
-        }
-
-        private void button5_Click(object sender, EventArgs e)
-        {
-            
-        
-        }
-        
     }
 }
